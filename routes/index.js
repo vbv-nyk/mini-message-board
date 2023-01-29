67410fd02fca62655831717f46ac9b9ced4d2cda
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-29 19:43:36 +0530

    Users can post messages and display them

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+router.get("/", function (req, res, next) {
+  res.render("index", { messages });
 });
-
+router.get("/new", (req, res, next) => {
+  res.render("form");
+});
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.body.Message,
+    user: req.body.AuthorName,
+    add: new Date(),
+  });
+  res.redirect("/");
+});
 module.exports = router;
